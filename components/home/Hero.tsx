650211e40b10fb10bf5c52fc4261d05b84d1f987
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2026-01-08 08:17:46 +0000

    populate hero with content and carousel

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,5 +1,29 @@
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import HeroCarousel from './HeroCarousel';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={sectionStyles}>
+      <div>
+        <h1 className={h1Styles}>We are changing the way people shop</h1>
+        <p className={paraStyles}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet tempora
+          aspernatur dolor, molestias rerum sequi molestiae iste eos odio
+          voluptatem dicta beatae nemo consequuntur distinctio doloribus sint
+          alias voluptates ipsum!
+        </p>
+        <Button asChild size='lg' className='mt-10'>
+          <Link href='/products'>Explore Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 };
 
 export default Hero;
+
+const sectionStyles = 'grid grid-cols-1 lg:grid-cols-2 gap-24 items-center';
+const h1Styles = 'max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl';
+const paraStyles = 'mt-8 max-w-xl text-lg leading-8 text-muted-foreground';
--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -1,5 +1,43 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel';
+import { Card, CardContent } from '@/components/ui/card';
+import Image from 'next/image';
+import hero1 from '@/public/images/hero1.jpg';
+import hero2 from '@/public/images/hero2.jpg';
+import hero3 from '@/public/images/hero3.jpg';
+import hero4 from '@/public/images/hero4.jpg';
+
+const carouselImages = [hero1, hero2, hero3, hero4];
+
 const HeroCarousel = () => {
-  return <div>HeroCarousel</div>;
+  return (
+    <div className='hidden lg:block'>
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((img, index) => {
+            return (
+              <CarouselItem key={index}>
+                <Card>
+                  <CardContent className='p-2'>
+                    <Image src={img} alt='hero' className={heroImgStyles} />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 };
 
 export default HeroCarousel;
+
+const heroImgStyles = 'w-full h-[24rem] rounded-md object-cover';
