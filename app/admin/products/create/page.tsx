cc6f63fffdcbb89a0bae238060d67ed86870e0e7
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2026-01-10 20:43:38 +0000

    set up all form-related components

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,5 +1,41 @@
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { faker } from '@faker-js/faker';
+import FormInput from '@/components/form/FormInput';
+
+const createProductAction = async (formData: FormData) => {
+  'use server';
+  const name = formData.get('name') as string;
+  console.log('name>>> ', name);
+};
+
 const CreateProductPage = () => {
-  return <div>Create Product Page</div>;
+  const name = faker.commerce.productName();
+  const company = faker.company.name();
+  const description = faker.lorem.paragraph({ min: 10, max: 12 });
+
+  return (
+    <section>
+      <h1 className={h1Styles}>create product</h1>
+      <div className={formDivStyles}>
+        <form action={createProductAction}>
+          <FormInput
+            name='name'
+            type='text'
+            label='product name'
+            defaultValue={name}
+          />
+          <Button type='submit' size='lg'>
+            Submit
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default CreateProductPage;
+
+const h1Styles = 'text-2xl font-semibold mb-8 capitalize';
+const formDivStyles = 'border p-8 rounded-md';
