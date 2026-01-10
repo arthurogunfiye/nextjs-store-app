14ca078573a5be66274e44a00aea509a97eef24c
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2026-01-10 04:43:40 +0000

    implemented sign out and user button components

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,26 @@
-const UserIcon = () => {
-  return <div>UserIcon</div>;
+import { LuUser } from 'react-icons/lu';
+import { currentUser } from '@clerk/nextjs/server';
+
+const UserIcon = async () => {
+  const user = await currentUser();
+
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        alt='user image'
+        className='size-6 rounded-full object-cover'
+      />
+    );
+  }
+
+  return (
+    <LuUser className='size-6 bg-primary rounded-full text-white'>
+      UserIcon
+    </LuUser>
+  );
 };
 
 export default UserIcon;
