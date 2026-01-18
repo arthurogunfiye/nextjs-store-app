294dfb6bf199fa8d46f142ef1d7f927906fe5ae6
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2026-01-18 23:36:38 +0000

    implemented creating and displaying product reviews

--- components/reviews/Rating.tsx
+++ components/reviews/Rating.tsx
@@ -1,5 +1,31 @@
-const Rating = () => {
-  return <div>Rating</div>;
+import { FaStar, FaRegStar } from 'react-icons/fa';
+
+const Rating = ({ rating }: { rating: number }) => {
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1 <= rating);
+
+  return (
+    <div className='flex items-center gap-x-1'>
+      {stars.map((isFilled, i) => {
+        const className = `size-3 ${
+          isFilled ? 'text-primary' : 'text-grey-400'
+        }`;
+        return isFilled ? (
+          <FaStar className={className} key={i} />
+        ) : (
+          <FaRegStar className={className} key={i} />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Rating;
+
+// Stars display logic
+// Let rating = 2
+// 1 <= 2 - true
+// 2 <= 2 - true
+// 3 <= 2 - false
+// 4 <= 2 - false
+// 5 <= 2 - false
+// If true display FaStar else display FaRegStar
