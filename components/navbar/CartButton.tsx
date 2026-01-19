d27617709c47bde1692c8cf46e68e985507ec908
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2026-01-19 22:04:16 +0000

    set up cart and cartItem models

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import { Button } from '../ui/button';
 import { LuShoppingCart } from 'react-icons/lu';
+import { fetchCartItems } from '@/utils/actions';
 
-const CartButton = () => {
-  const numberOfItemsInCart = 9;
+const CartButton = async () => {
+  const numberOfItemsInCart = await fetchCartItems();
   return (
     <Button asChild variant='outline' size='icon' className={btnStyles}>
       <Link href='/cart'>
